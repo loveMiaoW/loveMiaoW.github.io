4f1ec3754f9938191793795bc862361910f8a479
loveMiaoW <2338640164@qq.com>
2022-05-11 17:25:12 +0800

    0511

---
+++ js/docsifyinfo.js
@@ -0,0 +1,18 @@
+window.$docsify = {
+      name: '<p>🌈 loveMiaoW</p>',
+      repo: '',
+      themeColor: '#000000',
+      loadNavbar: true,
+      mergeNavbar: true,
+      externalLinkTarget: '_blank', 
+      maxLevel: 6,
+      loadSidebar: true,
+      alias: {
+          "./.*/_sidebar.md": "/_sidebar.md",
+        },
+        relativePath:true,
+        Valine: {
+              appId: 'RhHqMa9RPDHhPfTq809Wdced-gzGzoHsz',
+              appKey: 't16ozvHe1bfrnpzwoLOfg6P6'
+          }
+    }
